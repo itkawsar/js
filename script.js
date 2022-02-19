783ded9a248c2b4b91b3906cf337ebb5e1c61ce6
mkawsar <connectkawsar@gmail.com>
2022-02-19 10:53:40 +0600

    js added

---
+++ script.js
@@ -0,0 +1,655 @@
+//document.write("<h1> Hello Bangladesh </h1>");
+/*
+var x = 35;
+var marks = [24, 56, 67, 75, 34];
+
+var fruits = ['a', 'd', 'e', 't', 'y'];
+var mixed = ['str', 67, [5, 7]];
+var arr = new Array(23, 123, 21, 'orange');
+
+marks[0] = 98;
+var arelemnt = marks[1];
+document.write('element is:' + arelemnt + '<br>');
+document.write(marks.length + '<br>');
+document.write(marks[0] + '<br>');
+document.write(marks.indexOf(67) + '<br>');
+marks.push(35);
+marks.unshift(104);
+marks.splice(2, 4);
+marks.reverse();
+var marks2 = [1, 2, 3];
+marks = marks.concat(marks2);
+document.write(marks);
+
+let myObj = {
+    'first start': 'Kawsar',
+    channel: 'triplewebdesign',
+    isActive: true,
+    mark: [1, 2, 3, 4]
+}
+document.write(myObj.mark + '<br>');
+document.write(myObj['channel'] + '<br>');
+document.write(myObj['first start'] + '<br>');
+
+
+
+var myN = "Kawsar";
+var roL = 1;
+var obJ = {
+    start: 'Kavita',
+    roll: 2
+}
+var aRR = new Array(1, 22, 3);
+document.write(typeof(roL) + '<br>');
+document.write(typeof obJ + '<br>');
+
+
+
+
+var fstart = "This is my start";
+var lstart = "Kawsar";
+document.write(fstart + lstart + '<br>');
+document.write(fstart + ' ' + lstart + '<br>');
+fstart = fstart.concat(lstart);
+document.write(fstart + '<br>');
+
+
+var html;
+html = '<h2>This is my world</h2>' +
+    '<p>para</p>'
+document.write(html + '<br>');
+document.write(html.length + '<br>');
+
+
+var css;
+css = 'Cascading Style Sheet';
+document.write(css + '<br>');
+document.write(css.length + '<br>');
+document.write(css.toLowerCase() + '<br>');
+document.write(css.toLocaleUpperCase() + '<br>');
+document.write(css[1] + '<br>');
+document.write(css.indexOf('y') + '<br>');
+document.write(css.lastIndexOf('e') + '<br>');
+document.write(css.charAt(5) + '<br>');
+document.write(css.endsWith('y') + '<br>');
+document.write(css.includes('y') + '<br>');
+document.write(css.substring(1, 4) + '<br>');
+document.write(css.substring(4) + '<br>');
+document.write(css.slice(-4) + '<br>');
+document.write(css.slice(4) + '<br>');
+document.write(css.slice(0, 4) + '<br>');
+document.write(css.split('e') + '<br>');
+document.write(css.replace('Sheet', 'Formula') + '<br>');
+
+var starte = "Tanjila";
+var fruits = 'Banana\'s ';
+var fruits2 = "Strawberry";
+
+let htmlCss = ` Hello ${starte} 
+<h2> She loves to eat ${fruits2} </h2> 
+<p> and alos love to eat ${fruits} too
+`;
+document.write(htmlCss);
+
+
+var num = 34;
+document.write(typeof(num) + '<br>');
+num = String(num);
+document.write(typeof(num) + '<br>');
+var startes = "Faisal";
+document.write(startes.length, typeof(startes) + '<br>');
+// string and arrys length are not same
+var numeric = 56;
+document.write(numeric + '<br>');
+document.write(typeof(numeric) + '<br>');
+numeric = numeric.toString();
+document.write(numeric + '<br>');
+document.write(typeof numeric + '<br>');
+var nu = "3456";
+document.write(nu + '<br>');
+document.write(typeof(nu) + '<br>');
+nu = Number(nu);
+document.write(typeof(nu) + '<br>');
+
+var pro = "56465";
+pro = Number(pro);
+pro = Number(true);
+document.write(pro, typeof pro);
+
+
+var namber = "65.089";
+namber = parseInt(namber);
+document.write(namber, typeof namber + '<br>');
+// Number and parseInt have differnce to show data, Number show all the charter of the number with (.) on the other hand parseInt show only the number with (.)
+
+var fLoat = "87.098";
+fLoat = parseFloat(fLoat);
+document.write(fLoat, typeof fLoat + '<br>');
+document.write(fLoat.toFixed(), typeof fLoat + '<br>');
+document.write(fLoat.toFixed(3), typeof fLoat + '<br>');
+
+var bule = true;
+document.write(bule, typeof bule + '<br>');
+bule = String(true);
+document.write(bule, typeof bule + '<br>');
+
+// type coercion
+
+var fna = "7878";
+var tna = 67776;
+document.write(fna + tna);
+
+
+
+var age = 19;
+if (age == 20) {
+    document.write("Age is 20");
+} else if (age == 19) {
+    document.write("age is 19");
+
+} else {
+    document.write("Please correct your age");
+}
+
+
+document.write('<br>')
+
+
+var age = "25";
+if (age == 25) {
+    document.write("Age is 25");
+} else if (age == 19) {
+    document.write("age is 19");
+
+} else {
+    document.write("Please correct your age");
+}
+
+document.write('<br>')
+
+var age = "34";
+if (age == 25) {
+    document.write("Age is 25");
+} else if (age === 34) {
+    document.write("age is 34");
+
+} else {
+    document.write("Please correct your age");
+}
+
+document.write('<br>')
+
+var age = "47";
+if (age !== 47) {
+    document.write("Age is 47");
+} else if (age !== 34) {
+    document.write("age is 34");
+
+} else {
+    document.write("Please correct your age");
+}
+document.write('<br>')
+    //var vari = 34;
+if (typeof vari !== "undefined") {
+    document.write("variable is defined");
+} else {
+    document.write("variable is undefined");
+}
+
+var age = 14;
+switch (age) {
+    case 18:
+        document.write("You are 18");
+        break;
+    case 19:
+        document.write("You are 19");
+        break;
+    case 15:
+        document.write("You are 15");
+        break;
+    case 14:
+        document.write("You are 14");
+        break;
+
+    default:
+        document.write("Please correct your age")
+        break;
+}
+*/
+/*
+for (let i = 0; i < 100; i += 1) {
+    if (i === 7) {
+        break;
+    }
+    document.write(i + '<br>');
+
+}
+*/
+/*
+for (let i = 0; i < 10; i += 1) {
+    if (i === 7) {
+        continue;
+    }
+    document.write(i + '<br>');
+
+}
+*/
+/*
+for (let j = 0; j < 100; j += 1) {
+
+    document.write(j + '<br>');
+    if (j === 7) {
+        break;
+    }
+}
+*/
+/*
+var i = 0;
+while (i < 100) {
+    document.write((i + 1) + '<br>');
+    i += 1;
+}
+*/
+/*
+var i = 0;
+do {
+    document.write(i + '<br>');
+    if (i === 7) {
+        break;
+    }
+    i++;
+} while (i < 100)
+
+*/
+/*
+var aRR = new Array('a', 'b', 'c', 'd', 'e');
+aRR.forEach(function(element) {
+    document.write(element + '<br>')
+});
+
+
+var aRRt = new Array('a', 'b', 'c', 'd', 'e');
+aRRt.forEach(function(element, index, array) {
+    document.write(element, index, array + '<br>')
+});
+
+*/
+/*
+var aRRa = new Array('a', 'b', 'c', 'd', 'e', 'g');
+
+for (let k = 0; k < aRRa.length; k++) {
+    const element = aRRa[k];
+    document.write(element + '<br>')
+
+}
+*/
+/*
+let myObj = {
+    start: 'Kawsar',
+    channel: 'triplewebdesign',
+    isActive: true,
+    mark: [1, 2, 3, 4]
+}
+for (let key in myObj) {
+    document.write(`The ${key} of object is ${myObj[key]}` + '<br>');
+}
+*/
+
+/*
+var d = 67;
+var g;
+
+(d > 53) ? g = "true": g = "false";
+document.write(g);
+
+document.write('<br>');
+
+var a = 34;
+var b;
+b = (a > 45) ? 'true' : 'false';
+document.write(b);
+
+document.write('<br>');
+
+var h = 78;
+var t;
+t = "value is" + ' ' + ((h > 98) ? 'true' : 'false');
+document.write(t);
+
+
+var k = confirm("Do you like India");
+if (k) {
+    alert('Thanks');
+} else {
+    alert("Bye");
+}
+
+
+var m = prompt("What is your start ");
+document.write(m);
+
+
+
+function myNmae(start = "Kawsar", age = 34) {
+
+    document.write("My start is " + start + " and age is " + age);
+}
+
+
+function sum(a, b) {
+    document.write('The result is ' + (a + b))
+}
+myNmae("Iv", 29);
+document.write('<br>')
+sum(50, 30);
+
+document.write('<br>')
+
+
+function wholeNmae(fstart, lstart) {
+    var x = fstart + " " + lstart;
+    return x
+
+}
+var fn = wholeNmae("Mohammad", "Kawsar");
+document.write(fn);
+
+
+function mClick() {
+    alert("Hello Kawsar");
+
+}
+
+//var a = [1, 3, 4, 6, 7]
+document.write('<br>');
+for (var a = 1; a < 100; a = 10 + a) {
+    for (b = a; b < 10 + a; b++) {
+        document.write(b + " ");
+    }
+    document.write('<br>');
+}
+
+document.write('<br>');
+for (var a = 1; a <= 5; a++) {
+    for (b = 1; b <= a; b++) {
+        document.write(b);
+    }
+    document.write('<br>');
+}
+
+document.write('<br>');
+for (a = 1; a <= 5; a++) {
+    for (b = 1; b <= a; b++) {
+        document.write(a);
+    }
+    document.write('<br>');
+}
+
+document.write('<br>');
+for (var a = 5; a >= 1; a--) {
+    for (var b = 1; b <= a; b++) {
+        document.write(a);
+    }
+    document.write('<br>');
+}
+document.write('<br>');
+for (var a = 5; a >= 1; a--) {
+    for (var b = a; b >= a; b--) {
+        document.write(b);
+    }
+    document.write('<br>');
+}
+
+document.write("<br><br>");
+var ar = [12, 16, 17, 19, 20];
+var b = ar.filter(adult);
+
+document.write(b + "<br>");
+
+function adult(age) {
+    return age <= 18;
+}
+
+
+
+var starts = ["a", "b", "c", "d", ];
+starts.forEach(loop);
+
+function loop(nam, index) {
+    document.write(index + ":" + nam + "<br>")
+
+};
+
+document.write("<br><br>");
+
+var a = {
+    fstart: "Mohammad",
+    lstart: "Kawsar",
+    age: 35,
+    country: "Bangladesh",
+    arry: [1, 2, 3, 4],
+    salary: function() {
+        return 25000;
+    },
+    fullnam: function() {
+        return this.fstart + " " + this.lstart;
+    },
+    living: {
+        city: "Dhaka",
+        country: "Bangladesh"
+    }
+
+};
+
+document.write(a.living.country);
+
+
+
+document.write("<br><br>");
+
+var person = new Object();
+person.fstart = "Mohammad";
+person.lnme = 'Kawsar';
+document.write(person['fstart']);
+
+document.write("<br><br>");
+
+var student = [
+    { start: 'Kawsar', age: 35 },
+    { start: 'Rasel', age: 40 },
+    { start: 'Faisal', age: 34 },
+];
+
+for (let a = 0; a < student.length; a++) {
+    document.write(student[a].start + " " + student[a].age + "<br>");
+
+}
+
+document.write("<br><br>");
+
+const a = new Array(20, 30, 45);
+document.write(a);
+document.write('<br>');
+a[0] = 10;
+a[1] = 11;
+a[2] = 12;
+
+document.write(a);
+
+document.write('<br>');
+
+const student = {
+    start: "Kawsar",
+    age: 35,
+
+}
+student.start = "Raihan";
+student.age = 27;
+
+console.log(student);
+document.write(student['age']);
+
+
+
+
+document.write("<br><br>");
+
+var obj = {
+    fstart: "Mohammad",
+    lstart: "Kawsar",
+    age: 35,
+    email: "connectkawsar@gmail.com"
+};
+
+
+for (var key in obj) {
+    document.write(key + " : " + obj[key] + "<br>");
+}
+*/
+
+/*
+document.write("<br><br>");
+
+
+var ary = new Array(10, 20, 30, 40, 50);
+var multi = ary.map(test);
+document.write(multi);
+
+function test(x) {
+    return x * 10;
+}
+*/
+
+document.write("<br><br>");
+
+/*
+var student = [
+    { start: 'Kawsar', age: 35 },
+    { start: 'Rasel', age: 40 },
+    { start: 'Faisal', age: 34 },
+];
+var mult = student.map(test);
+document.write(mult);
+
+function test(y) {
+    return (y.start + y.age + "<br>");
+}
+
+
+var a = Math.ceil(2.6);
+document.write(a);
+
+document.write("<br>");
+
+var b = Math.floor(2.6);
+document.write(b);
+
+document.write("<br>");
+
+var b = Math.round(2.6);
+document.write(b);
+
+document.write("<br>");
+
+var b = Math.trunc(2.6);
+document.write(b);
+
+document.write("<br>");
+
+var b = Math.max(6, 15, 3, 59, 69, 12);
+document.write(b);
+
+document.write("<br>");
+
+var b = Math.min(6, 15, 3, 59, 69, 12);
+document.write(b);
+
+document.write("<br>");
+
+var b = Math.sqrt(9);
+document.write(b);
+
+document.write("<br>");
+
+var b = Math.cbrt(27);
+document.write(b);
+
+document.write("<br>");
+
+var b = Math.pow(2, 4);
+document.write(b);
+
+document.write("<br>");
+
+var d = Math.floor(Math.random() * 10) + 1;
+document.write(d);
+
+
+
+document.write("<br>");
+var tod = new Date();
+document.write(tod);
+document.write("<br>");
+document.write("<br>");
+
+var now = new Date();
+
+document.write(now.getDate());
+
+
+
+
+
+
+
+
+
+
+var element;
+
+element = document.getElementById("my-profile").innerHTML;
+
+console.log(element);
+*/
+document.write("<h2>Assignment Operator</h2>");
+var a = 10;
+var b = 3;
+document.write("Addition : " + (a + b) + "<br>");
+document.write("Subtraction : " + (a - b) + "<br>");
+document.write("Multiplication : " + (a * b) + "<br>");
+document.write("Division : " + (a / b) + "<br>");
+document.write("Modulus : " + (a % b) + "<br>");
+document.write("Exponention : " + (a ** b) + "<br>");
+
+document.write("<br>");
+document.write("<h2>Array</h2>");
+var start = ["Tonmoy", 'Faisal', 'Kawsar', "Tahmid", 'Raihan'];
+for (var i = 0; i < start.length; i++) {
+    document.write(start[i] + " <br>");
+}
+
+document.write("<br>");
+document.write("<h2>Even Or ODD</h2>");
+
+
+for (var z = 1; z <= 20; z++) {
+    if (z % 2 == 0) {
+        document.write(z + "<br>");
+    }
+}
+
+document.write("<br>");
+
+
+function g_option(selector, minvalue, maxvalue) {
+    var jsSelector = document.getElementById(selector);
+    for (var s = minvalue; s <= maxvalue; s++) {
+        var option = document.createElement("option");
+        var optiontext = document.createTextNode(s);
+        option.appendChild(optiontext);
+        jsSelector.appendChild(option);
+    }
+}
+g_option('year', 1950, 2022);
+g_option('month', 1, 12);
+g_option('day', 1, 31);
